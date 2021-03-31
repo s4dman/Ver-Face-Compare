f7cac879da9a91e652e24262f39297a798434b69
sadman hasan <sadman.h@live.com>
2021-03-31 04:58:24 -0700

    Added Ver-ID dependencies

---
+++ app/src/main/java/com/sadmanhasan/ver_face_compare/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sadmanhasan.ver_face_compare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
